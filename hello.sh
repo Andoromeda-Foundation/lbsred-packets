4d1ffff92b5d4c03feee549b31b9055fefb32b73
gaojin <1019855680@qq.com>
2018-06-10 12:49:59 +0800

    update

--- hello.sh
+++ hello.sh
@@ -1,16 +1,13 @@
 
 eosiocpp -o lbsred-packets.wast lbsred-packets.cpp
 eosiocpp -g lbsred-packets.abi lbsred-packets.cpp
-cleos create account eosio $1 EOS6zzuh8wUHAmEftGNzHLRDCaxtVmTdBKWNCMDb9rF3DhQMB1XuQ EOS6zzuh8wUHAmEftGNzHLRDCaxtVmTdBKWNCMDb9rF3DhQMB1XuQ
+cleos create account eosio $1 EOS57HV2rCxkx2fSfAMDLxKKy39kK4ZYfuK15kQiLMdJngj2PZ3S7 EOS57HV2rCxkx2fSfAMDLxKKy39kK4ZYfuK15kQiLMdJngj2PZ3S7
 cleos set contract $1  ../lbsred-packets -p $1
 cleos get table $1 $1 red
-cleos set account permission red4 active '{"threshold": 1,"keys": [{"key": "EOS6zzuh8wUHAmEftGNzHLRDCaxtVmTdBKWNCMDb9rF3DhQMB1XuQ","weight": 1}],"accounts":[{"permission":{"actor":"'$1'","permission":"eosio.code"},"weight":1}]}' owner -p red4
-
-cleos set account permission $1 active '{"threshold": 1,"keys": [{"key": "EOS6zzuh8wUHAmEftGNzHLRDCaxtVmTdBKWNCMDb9rF3DhQMB1XuQ","weight": 1}],"accounts":[{"permission":{"actor":"'$1'","permission":"eosio.code"},"weight":1}]}' owner -p $1
+cleos set account permission red4 active '{"threshold": 1,"keys": [{"key": "EOS57HV2rCxkx2fSfAMDLxKKy39kK4ZYfuK15kQiLMdJngj2PZ3S7","weight": 1}],"accounts":[{"permission":{"actor":"'$1'","permission":"eosio.code"},"weight":1}]}' owner -p red4
+cleos set account permission $1 active '{"threshold": 1,"keys": [{"key": "EOS57HV2rCxkx2fSfAMDLxKKy39kK4ZYfuK15kQiLMdJngj2PZ3S7","weight": 1}],"accounts":[{"permission":{"actor":"'$1'","permission":"eosio.code"},"weight":1}]}' owner -p $1
 cleos push action $1 sent '["red4","10.0000 EOS",10]' -p red4
 cleos get table $1 $1 red
-
-
 
 cleos push action $1 take '["red3",1]' -p red3
 cleos push action $1 take '["red2",1]' -p red2
